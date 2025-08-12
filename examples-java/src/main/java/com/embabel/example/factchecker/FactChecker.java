89935b8302b712505bd43e6e052d69ce6d8d445a
Rod Johnson <johnsonroda@gmail.com>
2025-08-12 14:33:48 +1000

    Multi LLM fact checker

--- examples-java/src/main/java/com/embabel/example/factchecker/FactChecker.java
+++ examples-java/src/main/java/com/embabel/example/factchecker/FactChecker.java
@@ -18,16 +18,17 @@ package com.embabel.example.factchecker;
 import com.embabel.agent.api.annotation.AchievesGoal;
 import com.embabel.agent.api.annotation.Action;
 import com.embabel.agent.api.annotation.Agent;
+import com.embabel.agent.api.annotation.Export;
 import com.embabel.agent.api.common.ActionContext;
 import com.embabel.agent.api.common.OperationContext;
 import com.embabel.agent.api.common.TransformationActionContext;
 import com.embabel.agent.api.common.workflow.control.ResultList;
 import com.embabel.agent.api.common.workflow.multimodel.ConsensusBuilder;
-import com.embabel.agent.config.models.OpenAiModels;
 import com.embabel.agent.core.CoreToolGroups;
 import com.embabel.agent.domain.io.UserInput;
 import com.embabel.agent.domain.library.InternetResource;
 import com.embabel.common.ai.model.LlmOptions;
+import com.embabel.common.ai.prompt.PromptContributor;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.google.common.base.Supplier;
 import org.slf4j.LoggerFactory;
@@ -35,13 +36,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-record FactualAssertion(String standaloneAssertion) {
+/**
+ * Non-overlapping factual assertions extracted from content.
+ *
+ * @param assertions
+ */
+record DistinctFactualAssertions(List<String> assertions) {
 }
 
-record DistinctFactualAssertions(List<FactualAssertion> assertions) {
-}
-
+/**
+ * Fact check of a single assertion.
+ */
 record FactCheck(
         String assertion,
         boolean isTrue,
@@ -54,7 +61,9 @@ record FactCheck(
 }
 
 record FactChecks(
-        List<FactCheck> checks
+        List<FactCheck> checks,
+        @JsonPropertyDescription("Source of the fact checks, typically a LLM model")
+        String source
 ) {
 }
 
@@ -69,16 +78,26 @@ record FactCheckerProperties(
         int maxConcurrency
 ) {
 
-    List<LlmOptions> llms() {
-        return models.stream()
-                .map(LlmOptions::withModel)
-                .map(buildable -> (LlmOptions) buildable)
-                .toList();
-    }
-
     LlmOptions deduplicationLlm() {
         return LlmOptions.withModel(deduplicationModel);
     }
+
+    /**
+     * Allows consistent exposure of relevant properties in prompts
+     */
+    PromptContributor promptContributor() {
+        return PromptContributor.fixed(
+                """
+                        Be guided by the following regarding sources:
+                        - Trusted sources: %s
+                        - Untrusted sources: %s
+                        """.formatted(
+                        String.join(", ", trustedSources),
+                        String.join(", ", untrustedSources)
+                )
+        );
+    }
+
 }
 
 @Agent(description = "Fact checker agent")
@@ -97,57 +116,68 @@ class FactChecker {
             ActionContext actionContext) {
         return ConsensusBuilder
                 .returning(DistinctFactualAssertions.class)
-                .sourcedFrom(generators(userInput, actionContext))
-                .withConsensusBy(this::getDistinctFactualAssertions)
+                .sourcedFrom(factualAssertionExtractors(userInput, actionContext).toList())
+                .withConsensusBy(this::consolidateFactualAssertions)
                 .asSubProcess(actionContext);
     }
 
 
-    @AchievesGoal(description = "Content has been fact-checked")
+    @AchievesGoal(description = "Content has been fact-checked",
+            export = @Export(remote = true, startingInputTypes = {UserInput.class}))
     @Action
-    FactChecks factChecks(
+    FactChecks runAndConsolidateFactChecks(
             DistinctFactualAssertions distinctFactualAssertions,
-            OperationContext context) {
-        var promptRunner = context.ai().withLlm(
-                        LlmOptions.withModel(OpenAiModels.GPT_41_MINI)
-                )
-                .withToolGroup(CoreToolGroups.WEB);
-        var checks = context.parallelMap(distinctFactualAssertions.assertions(), properties.maxConcurrency(), assertion ->
-                promptRunner.createObject(
-                        """
-                                Given the following assertion, check if it is true or false and explain why in %d words
-                                Express your confidence in your determination as a number between 0 and 1.
-                                Use web tools so you can cite information to support your conclusion.
-                                
-                                Be guided by the following regarding sources:
-                                - Trusted sources: %s
-                                - Untrusted sources: %s
-                                
-                                ASSERTION TO CHECK:
-                                %s
-                                """.formatted(
-                                properties.reasoningWordCount(),
-                                properties.trustedSources(),
-                                properties.untrustedSources(),
-                                assertion.standaloneAssertion()
-                        ),
-                        FactCheck.class
-                ));
-        return new FactChecks(checks);
+            ActionContext context) {
+        var llmFactChecks = properties.models().stream()
+                .flatMap(model -> factCheckWithSingleLlm(model, distinctFactualAssertions, context))
+                .toList();
+        return ConsensusBuilder
+                .returning(FactChecks.class)
+                .sourcedFrom(llmFactChecks)
+                .withConsensusBy(this::reconcileFactChecks)
+                .asSubProcess(context);
     }
 
-    private List<Supplier<DistinctFactualAssertions>> generators(UserInput userInput, ActionContext actionContext) {
-        return properties.llms().stream()
-                .map(llm -> (Supplier<DistinctFactualAssertions>) () -> generate(userInput, actionContext, llm))
-                .toList();
+    /**
+     * Fact-check the distinct factual assertions using a given LLM
+     */
+    private Stream<Supplier<FactChecks>> factCheckWithSingleLlm(
+            String model,
+            DistinctFactualAssertions distinctFactualAssertions,
+            OperationContext context) {
+        return context.parallelMap(distinctFactualAssertions.assertions(), properties.maxConcurrency(), assertion ->
+                        context.ai()
+                                .withLlm(model)
+                                .withPromptContributor(properties.promptContributor())
+                                .withTools(CoreToolGroups.WEB)
+                                .createObject(
+                                        """
+                                                Given the following assertion, check if it is true or false and explain why in %d words
+                                                Express your confidence in your determination as a number between 0 and 1.
+                                                Use web tools so you can cite information to support your conclusion.
+                                                
+                                                ASSERTION TO CHECK:
+                                                %s
+                                                """.formatted(
+                                                properties.reasoningWordCount(),
+                                                assertion
+                                        ),
+                                        FactCheck.class
+                                )).stream()
+                .map(check -> () -> new FactChecks(List.of(check), model));
+    }
+
+    private Stream<Supplier<DistinctFactualAssertions>> factualAssertionExtractors(UserInput userInput, ActionContext actionContext) {
+        return properties.models().stream()
+                .map(llm -> () -> extractFactualAssertionsWithModel(userInput, actionContext, llm));
     }
 
     /**
      * Generate distinct factual assertions from the user input using the given LLM
      */
-    private DistinctFactualAssertions generate(UserInput userInput, ActionContext context, LlmOptions llm) {
+    private DistinctFactualAssertions extractFactualAssertionsWithModel(UserInput userInput, ActionContext context, String model) {
         return context.ai()
-                .withLlm(llm)
+                .withLlm(model)
                 .createObject(
                         """
                                 Extract distinct factual assertions from the following text.
@@ -170,20 +200,18 @@ class FactChecker {
                 );
     }
 
-    private DistinctFactualAssertions getDistinctFactualAssertions(
+    private DistinctFactualAssertions consolidateFactualAssertions(
             TransformationActionContext<ResultList<DistinctFactualAssertions>, DistinctFactualAssertions> context) {
         var allAssertions = context.getInput().getResults().stream()
                 .flatMap(result -> result.assertions().stream())
                 .distinct()
                 .toList();
-        var assertionsContent = allAssertions.stream()
-                .map(FactualAssertion::standaloneAssertion)
-                .collect(Collectors.joining("\n"));
+        var assertionsContent = String.join("\n", allAssertions);
         return context.ai()
                 .withLlm(properties.deduplicationLlm())
                 .createObject(
                         """
-                                Your role is to consolidate different factual assertions into a single list,
+                                Consolidate different factual assertions into a single list,
                                 with no overlap.
                                 Each assertion you return should be clear and stand by itself.
                                 
@@ -203,7 +231,6 @@ class FactChecker {
                                 You should return:
                                 - "France is larger than Sweden."
                                 
-                                
                                 Consolidate the following potentially overlapping factual assertions into a single list.
                                 Each assertion should be expressed in at most %d words.
                                 
@@ -215,5 +242,44 @@ class FactChecker {
                 );
     }
 
+    /**
+     * Use the best LLM to reconcile the fact checks into a single list.
+     */
+    private FactChecks reconcileFactChecks(
+            TransformationActionContext<ResultList<FactChecks>, FactChecks> context) {
+        var formattedFactChecks = new StringBuilder();
+        for (var factCheck : context.getInput().getResults()) {
+            formattedFactChecks.append("Source: ").append(factCheck.source()).append("\n");
+            formattedFactChecks.append("- ").append(factCheck.checks().stream()
+                            .map(check ->
+                                    String.format("%s (%B with confidence %.2f)\nReasoning: %s\nLinks:\n%s",
+                                            check.assertion(), check.isTrue(), check.confidence(), check.reasoning(), check.links()))
+                            .collect(Collectors.joining("\n- ")))
+                    .append("\n\n");
+        }
+        return context.ai()
+                .withLlm(properties.deduplicationLlm())
+                .withTools(CoreToolGroups.WEB)
+                .withPromptContributor(properties.promptContributor())
+                .createObject(
+                        """
+                                Your role is to reconcile different fact checks into a single list.
+                                You must decide on the quality of each check and merge useful results.
+                                Your determination should be expressed in at most %d words.
+                                
+                                Your confidence should reflect the mix of confidence levels in the checks.
+                                If the checks disagree,you may perform your own research with the given tools.
+                                However, do NOT do this if the checks are consistent and all with high confidence.
+                                
+                                For each assertion, you should return a consolidated set of links, excluding only
+                                those that are duplicates or from untrusted sources.
+                                
+                                All checks:
+                                %s
+                                """
+                                .formatted(properties.reasoningWordCount(), formattedFactChecks),
+                        FactChecks.class
+                );
+    }
 
 }
