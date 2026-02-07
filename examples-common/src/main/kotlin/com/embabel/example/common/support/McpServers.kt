52c8d7698053e70caafb3e4cee64c6a339b7b246
alexheifetz <aheifetz@rocketmail.com>
2026-02-07 01:08:38 -0500

    Update NPX_LINUX and NPX_WINDOWS constants

--- examples-common/src/main/kotlin/com/embabel/example/common/support/McpServers.kt
+++ examples-common/src/main/kotlin/com/embabel/example/common/support/McpServers.kt
@@ -19,4 +19,8 @@ object McpServers {
     const val DOCKER: String = "docker-ce"
 
     const val DOCKER_DESKTOP: String = "docker-desktop"
+
+    const val NPX_WINDOWS: String = "npx-windows"
+
+    const val NPX_LINUX: String = "npx-linux"
 }
